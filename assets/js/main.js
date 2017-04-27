9bee3c26fd8285abc8d3ac115f2d709b5718044b
Nadia Cuadros <november138@hotmail.com>
2017-04-27 17:53:19 -0500

    trabajando ya en el javascript

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+function(event){
+  event.preventDefault();
+}
+var  nombre = document.getElementById("nombre");
+var apellido = document.getElementById("apellido");
+var dni = document.getElementById("dni");
+var cumple = document.getElementById("cumple");
+var celular = document.getElementById("celular");
+var telefono = document.getElementById("telefono");
+var clave = document.getElementById("clave");
+var confirmar = document.getElementById("confirmar");
+var coments = document.getElementById("comentarios");
+var checkbox = document.getElementById("checkbox");
