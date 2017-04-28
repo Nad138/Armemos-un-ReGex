6f69e2319ac13304b9c527c5e38a749b5845564c
Nadia Cuadros <november138@hotmail.com>
2017-04-28 11:48:50 -0500

    dandole las validaciones a los ítems del formulario mediante pattern

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,17 @@
-function(event){
+/*function(event){
   event.preventDefault();
 }
-var  nombre = document.getElementById("nombre");
-var apellido = document.getElementById("apellido");
-var dni = document.getElementById("dni");
-var cumple = document.getElementById("cumple");
-var celular = document.getElementById("celular");
-var telefono = document.getElementById("telefono");
-var clave = document.getElementById("clave");
-var confirmar = document.getElementById("confirmar");
-var coments = document.getElementById("comentarios");
-var checkbox = document.getElementById("checkbox");
+var  nombre = document.getElementById("nombre").value;
+var apellido = document.getElementById("apellido").value;
+var dni = document.getElementById("dni").value;
+var cumple = document.getElementById("cumple").value;
+var celular = document.getElementById("celular").value;
+var telefono = document.getElementById("telefono").value;
+var clave = document.getElementById("clave").value;
+var confirmar = document.getElementById("confirmar").value;
+var coments = document.getElementById("comentarios").value;
+var checkbox = document.getElementById("checkbox").value;
+
+// Regex : Expresiones regulares a usar //
+
+var regname =*/
