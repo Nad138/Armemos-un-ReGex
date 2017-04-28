13302cdeb934cdfbd44b223dba06f7f86c57b826
Nadia Cuadros <november138@hotmail.com>
2017-04-28 12:49:07 -0500

    revisando funcionalidad de validaciones

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,28 @@
-/*function(event){
-  event.preventDefault();
+
+function validar(){
+  var  nombre = document.getElementById("nombre").value;
+  var apellido = document.getElementById("apellido").value;
+  var dni = document.getElementById("dni").value;
+  var cumple = document.getElementById("cumple").value;
+  var celular = document.getElementById("celular").value;
+  var telefono = document.getElementById("telefono").value;
+  var clave = document.getElementById("clave").value;
+  var confirmar = document.getElementById("confirmar").value;
+  var coments = document.getElementById("comentarios").value;
+  var checkbox = document.getElementById("checkbox").value;
+
+  if(clave == confirmar){
+    this.nextElementSibling.nextElementSibling.innerText= " ";
+    return true;
+  }
+  else{
+    this.nextElementSibling.nextElementSibling.innerText= "No coinciden. Vuelva a ingresar su clave";
+    return false;
+  }
+
 }
-var  nombre = document.getElementById("nombre").value;
-var apellido = document.getElementById("apellido").value;
-var dni = document.getElementById("dni").value;
-var cumple = document.getElementById("cumple").value;
-var celular = document.getElementById("celular").value;
-var telefono = document.getElementById("telefono").value;
-var clave = document.getElementById("clave").value;
-var confirmar = document.getElementById("confirmar").value;
-var coments = document.getElementById("comentarios").value;
-var checkbox = document.getElementById("checkbox").value;
+
 
 // Regex : Expresiones regulares a usar //
 
-var regname =*/
+var regname;
